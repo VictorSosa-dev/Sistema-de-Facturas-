587d014da263fc124b01461a8ca0c01db929928a
Victor Manuel Sosa Piña <73664479+VictorSosa-dev@users.noreply.github.com>
2022-03-16 22:01:00 -0600

    primer commit

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,76 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
+import com.bolsadeideas.springboot.app.models.service.JpaUserDetailsService;
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private JpaUserDetailsService userDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale", "/api/clientes/**").permitAll()
+//		.antMatchers("/ver/**").hasAnyRole("USER")
+//		.antMatchers("/uploads/**").hasAnyRole("USER")
+//		.antMatchers("/form/**").hasAnyRole("ADMIN")
+//		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+//		.antMatchers("/factura/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		    .formLogin()
+		        .successHandler(successHandler)
+		        .loginPage("/login")
+		    .permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403");
+	}
+	
+	
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		/*
+		 * Deprecated
+		 * UserBuilder users = User.withDefaultPasswordEncoder();
+		 * */
+		
+		build.userDetailsService(userDetailsService)
+		.passwordEncoder(passwordEncoder);
+
+		/*
+		build.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id = u.id) where u.username=?");
+		*/
+		
+		
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+//		
+//		build.inMemoryAuthentication()
+//		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+//		.withUser(users.username("victor").password("12345").roles("USER"));
+	}
+}
